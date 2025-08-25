8f3b93af76d122aa493c9ff0b370c2cda9f88458
Sharif2023 <sharifislam0505@gmail.com>
2025-08-25 11:56:35 +0600

    route of landing page

--- study-nest/src/App.jsx
+++ study-nest/src/App.jsx
@@ -1,5 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
