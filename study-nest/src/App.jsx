3d39b26093dd704d2f64bbc4e635d447156e33d5
Sharif2023 <sharifislam0505@gmail.com>
2025-08-28 10:46:20 +0600

    set route home

--- study-nest/src/App.jsx
+++ study-nest/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
+import Home from "./Pages/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
